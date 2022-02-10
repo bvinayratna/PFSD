391bbad16e96c5af720c03ffd3bb9a410ae1304c
bvinayratna <bezzam.vinay@gmail.com>
2022-02-10 13:44:51 +0530

    Course pages

--- CO2_cmd/erp.py
+++ CO2_cmd/erp.py
@@ -3,20 +3,48 @@ from turtle import title
 from flask import render_template
 from distutils.log import debug
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+semesters = ["Select Semester","Odd Sem", "Even Sem", "Summer Term"]
+
+years =[{"year": "Select Academic year"},{"year":"2021-2022"},
+{"year":"2020-2021"},
+{"year":"2019-2020"}]
+
 students = [{"regdno": 2000031242, "name": "Vinay", "course": "PFSD"},
 {"regdno": 2000030558, "name": "Sai", "course": "OSD"},
-{"regdno": 2000031251, "name": "Hecker Harsha Sins", "course": "AIDS"}]
+{"regdno": 2000031251, "name": "Hecker", "course": "Hacking"}]
 
 @app.route("/")
 def home():
-    return render_template("home.html", students = students)
+    return render_template("home.html", students = students,title="Home")
+
+@app.route("/courses")
+def courses():
+    return render_template("course_pages/courses.html",title="Courses", years=years,semesters=semesters, title2="materials", title3="assignments", title4="internals")
 
 @app.route("/login")
 def login():
     return render_template("login.html", title="Login")
+
+@app.route("/handouts")
+def handouts():
+    return render_template("course_pages/handouts.html", years=years,semesters=semesters,title="Handouts",)
+
+@app.route("/materials")
+def materials():
+    return render_template("course_pages/materials.html", years=years,semesters=semesters,title="Materials",)
+
+@app.route("/assignments")
+def assignments():
+    return render_template("course_pages/assignments.html", years=years,semesters=semesters,title="Assignments",)
+
+@app.route("/internals")
+def internals():
+    return render_template("course_pages/internals.html", years=years,semesters=semesters,title="Internals",)
 
 @app.route("/register")
 def register():
@@ -24,19 +52,19 @@ def register():
 
 @app.route("/logout")
 def logout():
-    return "<h1>Your are logged out</h1>"
+    return "<p>Your are logged out! <br>Thank you</p>"
 
 @app.route("/attendance")
 def attendance():
-    return "<p>view attendance or marks</p>"
+    return render_template("attendance.html", title="Attendance")
 
 @app.route("/profile")
 def profile():
-    return "<p>this is going to be your profile</p>"
+    return render_template("profile.html", title="Profile")
 
 @app.route("/contactus")
 def contactus():
-    return "<p>contact us</p>"
+    return render_template("contactus.html", title="Contact-us")
 
 if __name__ == "__main__":
     app.run()
--- csv1.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-f = open("product.csv", "r")
-data = csv.reader(f)
-print(data)
-
-for s in data:
-    print(s)
